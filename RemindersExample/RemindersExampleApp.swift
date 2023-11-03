ef6b768477f52cf8b007e6eaf8383989d1500282
Xinlin Zhou <XZhou@lcs.on.ca>
2023-11-03 11:58:33 -0400

    Initial Commit

---
+++ RemindersExample/RemindersExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersExampleApp.swift
+//  RemindersExample
+//
+//  Created by Fiona ZHOU on 2023-11-03.
+//
+
+import SwiftUI
+
+@main
+struct RemindersExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
