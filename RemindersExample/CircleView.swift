6cae6a1f1d8bdf01923939d18f2d153d4374889d
Xinlin Zhou <XZhou@lcs.on.ca>
2023-11-04 09:31:48 -0400

    Completed and constructed the first layer and put in all informations, need to figure out the navigation bar after.

---
+++ RemindersExample/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  RemindersExample
+//
+//  Created by Fiona ZHOU on 2023-11-03.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
---
+++ RemindersExample/RemainderListView.swift
@@ -0,0 +1,18 @@
+//
+//  RemainderListView.swift
+//  RemindersExample
+//
+//  Created by Fiona ZHOU on 2023-11-03.
+//
+
+import SwiftUI
+
+struct RemainderListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RemainderListView()
+}
